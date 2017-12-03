d605cd704b310a522239ec8a1c640121b7a0ad9d
YasminMattos <yasmin.mattos.conislla@gmail.com>
2017-12-03 00:29:11 -0500

    arregla bug

---
+++ README.md
@@ -0,0 +1,14 @@
+# **Tarjeta de Crédito Válida**
+
+------
+
+- ## **Objetivo**
+
+    Crear una web que verifique la validez de un número de tarjeta de crédito ingresado. Esto según el *algoritmo de Luhn*.
+
+- ## **Procedimiento**
+
+    La siguiente imagen es referencial y muestra el proceso del ***Algortimo de Luhn*** para validar una tarjeta.
+
+    ![luhn](assets/images/luhn_algorithm.png)
+
--- js/app.js
+++ js/app.js
@@ -1,48 +1,53 @@
 var btn = document.getElementById('btn');
 btn.addEventListener('click', isValidCard);
 
+function isValidCard(event) {
+  var cardNumber = document.getElementById('input').value;
+  var text = document.getElementById('output');
 
-function isValidCard(cardNumber) {
-  var cardNumber, text;
-  cardNumber = document.getElementById('input').value;
-  var arr = cardNumber.toString().split('').reverse();
-  var newArr = [];
-  
-  for(var i = 0; i < arr.length; i++)
-    newArr.push(parseInt(arr[i]));
+  if ( cardNumber == 0 || cardNumber == ''){
+    text.textContent = '';
+  } else {
+    var arr = cardNumber.toString().split('').reverse();
+    var newArr = [];
     
-    var even = [];//  [ 18, 4, 6, 16, 2 ]
-    var sumEven = [];//[9, 7, 4, 6, 2]
-    var odd = []; //  [ 4, 9, 7, 9, 7, 6 ]
-    
-    for(var j = 0; j < newArr.length; j++){
-    if((j+1) % 2 === 0){
-      even.push(newArr[j]*2);
-    } else{
-      odd.push(newArr[j]);
-    }
-    }
-    
-    for(var k = 0; k < even.length; k++){
-        if (even[k] >= 10){
-          sumEven.push(parseInt(even[k]/ 10) + even[k] % 10);
-        }else if(even[k] < 10){
-          sumEven.push(even[k]);          
-        }
-    }
-    
-    var mixArr = sumEven.concat(odd);
-    var totalSum = 0;
-    for(var m=0; m < mixArr.length; m++)
-      totalSum += mixArr[m];
+    for(var i = 0; i < arr.length; i++)
+      newArr.push(parseInt(arr[i]));
       
-      if(totalSum % 10 === 0){
-        text = 'The credit card N° ' + cardNumber + ' is valid';
-      } else {
-        text = 'Not valid credit card';
+      var even = [];//  [ 18, 4, 6, 16, 2 ]
+      var sumEven = [];//[9, 7, 4, 6, 2]
+      var odd = []; //  [ 4, 9, 7, 9, 7, 6 ]
+      
+      for(var j = 0; j < newArr.length; j++){
+      if((j+1) % 2 === 0){
+        even.push(newArr[j]*2);
+      } else{
+        odd.push(newArr[j]);
       }
+      };
       
-      document.getElementById('output').innerHTML = text;
-}
+      for(var k = 0; k < even.length; k++){
+          if (even[k] >= 10){
+            sumEven.push(parseInt(even[k]/ 10) + even[k] % 10);
+          }else if(even[k] < 10){
+            sumEven.push(even[k]);          
+          }
+      };
+
+      var mixArr = sumEven.concat(odd);
+      var totalSum = 0;
+      for(var m=0; m < mixArr.length; m++)
+        totalSum += mixArr[m];
+        
+        if(totalSum % 10 === 0 && cardNumber.length >= 9){
+          text.textContent = 'The credit card N° ' + cardNumber + ' is valid';
+        } else {
+          text.textContent = 'Not valid credit card';
+        };
+  };
+    
+  };
+  
+
  
-isValidCard(cardNumber);
+
